8daa8ae6abef27d814c06aa85c5cc164916ecd9a
Nagesh96 <103498281+Nagesh96@users.noreply.github.com>
2023-12-18 16:02:29 +0530

    Update new_release-1.py

--- new_release-1.py
+++ new_release-1.py
@@ -1,20 +1,47 @@
-from CherwellAPI import CherwellClient
+import requests
 
-# Create a new CherwellClient Connection
-cherwell_client = CherwellClient.Connection(<base_uri>,<api_key>,<username>,<password>)
+# Replace these with your Cherwell API details
+cherwell_url = "https://your-cherwell-instance"
+username = "your_username"
+password = "your_password"
+api_key = "your_api_key"  # If your Cherwell instance requires an API key
 
-# Create a new instance of a Release business object
-release = cherwell_client.get_new_business_object("Release")
+# API endpoint for creating a new Release
+api_endpoint = f"{cherwell_url}/api/V1/your_release_endpoint"
 
-# Set the properties of the new Release
-release.Description = "This is a test Release"
-release.Status = "Scheduled"
-release.ReleaseType = "Standard"
-release.ReleaseName = "Test Release"
+# Specify the headers
+headers = {
+    "Content-Type": "application/json",
+    "Authorization": f"Basic {api_key}" if api_key else None
+}
 
-# Save the new Release
-release.Save()
+# Specify the data for the new Release
+release_data = {
+    "ShortDescription": "Mobile deployment prod",
+    "Requestor": "Nagisetti, Sheela (P4567)",
+    "RequestGroup": "Spectrum Mobile App Support",
+    "Program": "Mobile 2.0",
+    "Environment": "prod",
+    "PrimaryApplicationCI": "MOBILE 2.0 Back office",
+    "ReleaseType": "Code",
+    "TypeOfTesting": "Sanity and Smoke Test",
+    "CurrentPlannedStartDate": "2023-12-20",
+    "DeploymentDescriptionSummary": "Defect Fixes",
+    "Build": "1.1.350",
+    "Urgency": "High",
+    "UrgencyReason": "Prod Deployment",
+    "ServiceImpact": "Yes - Continuous",
+    "ImpactToOrderInProgress": "Yes"
+}
 
-# Show the new business object record id
-print("RecId for new Release: {}".format(release.busObRecId))
-print("PublicId for new Release: {}".format(release.busObPublicId))
+# Send HTTP POST request to create a new Release
+try:
+    response = requests.post(api_endpoint, json=release_data, headers=headers, auth=(username, password))
+    response.raise_for_status()  # Raise an exception for HTTP errors
+
+    # Print the response details
+    print("New Release created successfully!")
+    print("Response Status Code:", response.status_code)
+    print("Response Content:", response.text)
+except requests.exceptions.RequestException as e:
+    print(f"Failed to create Release. An error occurred: {e}")

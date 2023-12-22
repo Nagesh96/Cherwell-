b140dcd884d76b270c3872f128470ba7fcc143f3
Nagesh96 <103498281+Nagesh96@users.noreply.github.com>
2023-12-22 16:39:56 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,61 +1,68 @@
 import requests
-import base64
 import json
 
-def create_cherwell_release(api_url, username, password, release_data):
-    # Endpoint for creating a new release
-    endpoint = f"{api_url}/api/V1/object/release"
+# Set the Cherwell API endpoint URL
+url = "https://charter.cherwellondemand.com/CherwellClient/CherwellAPI/api/V1/savebusinessobject"
 
-    # Set up headers with content type and authorization
-    headers = {
-        "Content-Type": "application/json",
-        "Authorization": get_authorization_header(username, password)
-    }
+# Set the Cherwell API key
+api_key = "<api_key>"
 
-    # Make a POST request to create a new release
-    response = requests.post(endpoint, headers=headers, json=release_data)
+# Set the Cherwell username and password
+username = "<username>"
+password = "<password>"
 
-    # Check if the release creation was successful (status code 201)
-    if response.status_code == 201:
-        print("New release created successfully!")
-        # Extract busObRecId and busObPublicId from the response
-        response_data = response.json()
-        busObRecId = response_data.get('busObRecId')
-        busObPublicId = response_data.get('busObPublicId')
-        print(f"busObRecId: {busObRecId}, busObPublicId: {busObPublicId}")
-        return busObRecId, busObPublicId
-    else:
-        print(f"Failed to create a new release. Status code: {response.status_code}")
-        print(f"Response content: {response.text}")
-        return None, None
-
-def get_authorization_header(username, password):
-    # Helper function to generate the Authorization header
-    credentials = f"{username}:{password}"
-    encoded_credentials = base64.b64encode(credentials.encode('utf-8')).decode('utf-8')
-    return f"Basic {encoded_credentials}"
-
-# Example usage
-api_url = ""
-username = ""
-password = ""
-
-release_data = {
-    "Short Description": "Providing Dummy Data for Testing purpose",
-    "Requestor": "Nageswara",
-    "Request Group": "Spectrum Mobile App Support",
-    "Program": "Mobile 2.0",
-    "Environment": "QA",
-    "Primary Application (CI)": "MOBILE 2.0 Back office",
-    "Release Type": "Code",
-    "TypeOfTesting": "Smoke Test only",
-    "Current Planned Start Date": "12-23-2023 02:59 PM",
-    "Deployment Description Summary": "Defect Fixes",
-    "Build": "1.0.1",
-    "Urgency": "Low",
-    "Urgency Reason": "Defect Fixes",
-    "Service Impact": "Yes - Continuous",
-    "Impacts to Orders in Progress": "Yes"
+# Set the Release properties
+release = {
+    "busObId": "Release",
+    "fields": [
+        {
+            "dirty": True,
+            "Short Description": "Providing Dummy Data for Testing purpose",
+            "Requestor": "annem, nageswara (P3214461)",
+            "Request Group": "Spectrum Mobile App Support",
+            "Program": "Spectrum Mobile 2.0",
+            "Environment": "QA",
+            "Primary Application (CI)": "SPECTRUM MOBILE 2.0 BACKOFFICE (SMBO M2) QA2",
+            "Release Type": "Code",
+            "Type Of Testing- Required": "Smoke Test only",
+            "Deployment Description Summary": "Defect Fixes",
+            "Build": "1.0.1",
+            "Urgency": "Low",
+            "Urgency Reason": "Defect Fixes",
+            "Service Impact": "Yes - Continuous",
+            "Impacts to Orders in Procress": "Yes"
+        }
+    ],
+    "persist": True
 }
 
-busObRecId, busObPublicId = create_cherwell_release(api_url, username, password, release_data)
+# Set the headers for the Cherwell API request
+headers = {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+    "Authorization": "Bearer " + api_key
+}
+
+# Authenticate with the Cherwell API
+auth_data = {
+    "grant_type": "password",
+    "client_id": "api_client",
+    "username": username,
+    "password": password
+}
+auth_url = "https://<base_uri>/CherwellAPI/token"
+auth_response = requests.post(auth_url, data=auth_data)
+auth_response.raise_for_status()
+auth_json = auth_response.json()
+api_key = auth_json["access_token"]
+
+# Set the headers for the Cherwell API request with the new API key
+headers["Authorization"] = "Bearer " + api_key
+
+# Create the new Release in Cherwell
+response = requests.post(url, headers=headers, data=json.dumps(release))
+response.raise_for_status()
+
+# Show the new business object record id
+print("RecId for new Release: {}".format(response.json()["busObRecId"]))
+print("PublicId for new Release: {}".format(response.json()["busObPublicId"]))

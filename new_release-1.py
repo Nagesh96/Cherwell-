d8d54175b96848612ea0454ba89215bc1b1927c8
Nagesh96 <103498281+Nagesh96@users.noreply.github.com>
2023-12-22 14:30:26 +0530

    Update new_release-1.py

--- new_release-1.py
+++ new_release-1.py
@@ -1,47 +1,48 @@
 import requests
+import base64
+import json
 
-# Replace these with your Cherwell API details
-cherwell_url = "https://your-cherwell-instance"
-username = "your_username"
-password = "your_password"
-api_key = "your_api_key"  # If your Cherwell instance requires an API key
+def create_cherwell_release(api_url, username, password, release_data):
+    # Endpoint for creating a new release (adjust based on Cherwell's API)
+    endpoint = f"{api_url}/api/V1/releases"
 
-# API endpoint for creating a new Release
-api_endpoint = f"{cherwell_url}/api/V1/your_release_endpoint"
+    # Set up headers with content type and authorization
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": get_authorization_header(username, password)
+    }
 
-# Specify the headers
-headers = {
-    "Content-Type": "application/json",
-    "Authorization": f"Basic {api_key}" if api_key else None
-}
+    # Make a POST request to create a new release
+    response = requests.post(endpoint, headers=headers, data=json.dumps(release_data))
 
-# Specify the data for the new Release
+    # Check if the release creation was successful (status code 201)
+    if response.status_code == 201:
+        print("New release created successfully!")
+        # Extract busObRecId and busObPublicId from the response
+        response_data = response.json()
+        busObRecId = response_data.get('busObRecId')
+        busObPublicId = response_data.get('busObPublicId')
+        print(f"busObRecId: {busObRecId}, busObPublicId: {busObPublicId}")
+        return busObRecId, busObPublicId
+    else:
+        print(f"Failed to create a new release. Status code: {response.status_code}")
+        return None, None
+
+def get_authorization_header(username, password):
+    # Helper function to generate the Authorization header
+    credentials = f"{username}:{password}"
+    encoded_credentials = base64.b64encode(credentials.encode('utf-8')).decode('utf-8')
+    return f"Basic {encoded_credentials}"
+
+# Example usage
+api_url = ""  # Adjust the URL
+username = ""
+password = ""
+
 release_data = {
-    "ShortDescription": "Mobile deployment prod",
-    "Requestor": "Nagisetti, Sheela (P4567)",
-    "RequestGroup": "Spectrum Mobile App Support",
-    "Program": "Mobile 2.0",
-    "Environment": "prod",
-    "PrimaryApplicationCI": "MOBILE 2.0 Back office",
-    "ReleaseType": "Code",
-    "TypeOfTesting": "Sanity and Smoke Test",
-    "CurrentPlannedStartDate": "2023-12-20",
-    "DeploymentDescriptionSummary": "Defect Fixes",
-    "Build": "1.1.350",
-    "Urgency": "High",
-    "UrgencyReason": "Prod Deployment",
-    "ServiceImpact": "Yes - Continuous",
-    "ImpactToOrderInProgress": "Yes"
+    "name": "Release 1.0",
+    "description": "This is the first release of the project.",
+    # Add more relevant data based on Cherwell's API documentation
 }
 
-# Send HTTP POST request to create a new Release
-try:
-    response = requests.post(api_endpoint, json=release_data, headers=headers, auth=(username, password))
-    response.raise_for_status()  # Raise an exception for HTTP errors
-
-    # Print the response details
-    print("New Release created successfully!")
-    print("Response Status Code:", response.status_code)
-    print("Response Content:", response.text)
-except requests.exceptions.RequestException as e:
-    print(f"Failed to create Release. An error occurred: {e}")
+busObRecId, busObPublicId = create_cherwell_release(api_url, username, password, release_data)
